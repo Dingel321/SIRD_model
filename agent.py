416b7540e0c9046d3f60038f8662a4eb81cf1ce3
Lars <76914948+Dingel321@users.noreply.github.com>
2022-01-28 18:36:50 +0100

    Results from first session, added agent class

---
+++ agent.py
@@ -0,0 +1,25 @@
+class Agent():
+
+    def __init__(self, status, infected_days) -> None:
+        self._status = status
+        self._days_of_sick = 0
+        self._infected_days = infected_days
+          
+    def get_status(self) -> str:
+        return self._status
+
+    def set_status(self, new_status) -> None:
+        if self._status == 'suseptible' and new_status == 'recoverd':
+            raise Exception('Wrong agent status!')
+        self._status = new_status
+
+    def add_sick_day(self) -> None:
+        if self._status == 'suseptible':
+            self._days_of_sick += 1
+        if self._days_of_sick > self._infected_days:
+            self._reset_sick_days()
+            
+    def _reset_sick_days(self) -> None:
+        self._days_of_sick = 0
+        
+
---
+++ enviroment.py
@@ -0,0 +1,5 @@
+class Enviroment():
+
+    def __init__(self) -> None:
+        pass
+
