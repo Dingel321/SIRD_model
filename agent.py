dd94f32634ca368a62e4a81d73ca16fa458ae4c7
Dingel321 <Dingel321@gmail.com>
2022-02-04 13:34:21 +0100

    added docstrings

--- agent.py
+++ agent.py
@@ -6,14 +6,19 @@ class Agent():
         self._infected_days = infected_days
           
     def get_status(self) -> str:
+        '''Returns current status of the agent'''
+
         return self._status
 
     def set_status(self, new_status) -> None:
+        '''Set The new current state of the agent'''
+
         if self._status == 'suseptible' and new_status == 'recoverd':
             raise Exception('Wrong agent status!')
         self._status = new_status
 
     def add_sick_day(self) -> None:
+        '''Increment days '''
         if self._status == 'suseptible':
             self._days_of_sick += 1
         if self._days_of_sick > self._infected_days:
